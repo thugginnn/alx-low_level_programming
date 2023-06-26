29ef6cb30e438fac44e6b3a6cdd8421f14ebccf7
thugginnn <paul.iyiade4@gmail.com>
2023-06-26 10:01:27 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - generates keygen.
- * Return: 0 Always.
- */
-int main(void)
-{
-	int r = 0, c = 0;
-	time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+#define PASSWORD_LENGTH 10
+
+char *generatePassword() {
+    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));  // Allocate memory for password
+
+    // Characters to be used in the password
+    const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate random characters for the password
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int randomIndex = rand() % (sizeof(characters) - 1);
+        password[i] = characters[randomIndex];
+    }
+
+    password[PASSWORD_LENGTH] = '\0';  // Add null terminator
+
+    return password;
+}
+
+int main() {
+    char *password = generatePassword();
+
+    printf("Generated Password: %s\n", password);
+
+    free(password);  // Free the allocated memory
+
+    return 0;
 }
