d71c5471268d0e0f696dbe9f6698dce5847df09d
thugginnn <paul.iyiade4@gmail.com>
2024-02-07 00:08:30 -0800

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,8 +5,8 @@
  * integers using the Linear search algorithm
  *
  * @array: input array
- * @size: size of the array
- * @value: value to search in
+ * @size: number of elements in the array
+ * @value: value to search for
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
